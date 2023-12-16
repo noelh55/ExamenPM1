73a084fa5f2aa473eb1ba507eb741205f72e03c2
noelh55 <hnoel1149@gmail.com>
2023-12-15 20:47:15 -0600

    15/12/2023

---
+++ app/src/main/java/hn/uth/appentrevista/Activity_lista_fotos.java
@@ -0,0 +1,14 @@
+package hn.uth.appentrevista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_lista_fotos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_fotos);
+    }
+}
